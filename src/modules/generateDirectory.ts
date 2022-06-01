700a6b62d5efb155401d62b2bd06f43504a81c4c
Sthanasis <sthanasis1994@gmail.com>
2022-06-01 20:32:49 +0300

    feat: add capitalize utility method, folder restructure

--- src/modules/generateDirectory.ts
+++ src/modules/generateDirectory.ts
@@ -2,7 +2,42 @@ import * as vscode from 'vscode';
 import * as fs from 'fs';
 import * as path from 'path';
 
-export async function generateDirectoryOnUserClick(uri?: vscode.Uri) {
+import { capitalize } from './utilities';
+
+function generateComponent(targetPath: string, cmpName: string) {
+  const capitalizedName = capitalize(cmpName);
+
+  const componentDirectory = path.join(targetPath, capitalizedName);
+  fs.mkdirSync(componentDirectory);
+
+  const componentIndexFile = path.join(componentDirectory, 'index.js');
+  const componentNamedFile = path.join(
+    componentDirectory,
+    `${capitalizedName}.js`
+  );
+
+  fs.writeFileSync(
+    componentIndexFile,
+    `export { default } from './${capitalizedName}';`
+  );
+  fs.writeFileSync(
+    componentNamedFile,
+    `import React from 'react';\n\nconst ${capitalizedName} = () => {\n  return <></>;\n};\n\nexport default ${capitalizedName};`
+  );
+}
+
+function getCurrentWorkspacePath(): vscode.WorkspaceFolder | undefined {
+  if (vscode.workspace.workspaceFolders) {
+    const workspaceDir = vscode.workspace.workspaceFolders[0].uri.fsPath;
+    const workspace = vscode.workspace.getWorkspaceFolder(
+      vscode.Uri.file(workspaceDir)
+    );
+    return workspace;
+  }
+  return undefined;
+}
+
+async function generateDirectoryOnUserClick(uri?: vscode.Uri) {
   try {
     let target: string | undefined;
 
@@ -35,30 +70,4 @@ export async function generateDirectoryOnUserClick(uri?: vscode.Uri) {
   }
 }
 
-function generateComponent(targetPath: string, cmpName: string) {
-  const componentDirectory = path.join(targetPath, cmpName);
-  fs.mkdirSync(componentDirectory);
-
-  const componentIndexFile = path.join(componentDirectory, 'index.js');
-  const componentNamedFile = path.join(componentDirectory, `${cmpName}.js`);
-
-  fs.writeFileSync(
-    componentIndexFile,
-    `export { default } from './${cmpName}';`
-  );
-  fs.writeFileSync(
-    componentNamedFile,
-    `import React from 'react';\n\nconst ${cmpName} = () => {\n  return <></>;\n};\n\nexport default ${cmpName};`
-  );
-}
-
-function getCurrentWorkspacePath(): vscode.WorkspaceFolder | undefined {
-  if (vscode.workspace.workspaceFolders) {
-    const workspaceDir = vscode.workspace.workspaceFolders[0].uri.fsPath;
-    const workspace = vscode.workspace.getWorkspaceFolder(
-      vscode.Uri.file(workspaceDir)
-    );
-    return workspace;
-  }
-  return undefined;
-}
+export default generateDirectoryOnUserClick;
